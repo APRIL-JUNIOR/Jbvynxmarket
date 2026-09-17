19e0d6145b55f634bac8fff1ec432d995fa93465
APRIL-JUNIOR <drgapriljunior@gmail.com>
2026-09-17 18:51:36 +0700

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,14 @@
+/* ============================================
+   KONFIGURASI VYNX STORE
+   Isi 3 nilai di bawah sesuai akun JSONBin.io kamu
+   ============================================ */
+const VYNX = {
+  // Dapatkan dari jsonbin.io setelah buat Bin (lihat langkah setup)
+  BIN_ID: "6aabc4c8ffd5d160531181f3",
+
+  // Dapatkan dari jsonbin.io → menu API Keys
+  MASTER_KEY: "$2a$10$H/lhoT5N57HDY/SOYrEsLODi038H7q/rzNQVzxQvCWtq0.8salHOm",
+
+  // Password untuk masuk admin.html (GANTI!)
+  ADMIN_PASS: "APRIL2013",
+};
